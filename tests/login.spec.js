c0f40bf58e42e2baf6ee486b350b67d90061be31
Kannatvr <kannadasanengg1@gmail.com>
2025-06-13 12:44:49 +0530

    empty commit

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -4,34 +4,21 @@ import { LoginPage } from '../pages/LoginPage';
 import { testData } from '../data/testData.js';
 
 test.describe('Login Scenarios', () => {
-  test('Valid login', async ({ page }) => {
+  
+  test('Login with Valid Credentials', async ({ page }) => {
     const loginPage = new LoginPage(page);
     const loginSuccess = await loginPage.login(testData.validUser.username, testData.validUser.password);
     expect(loginSuccess).toBe(true);
     await expect(page).toHaveURL(/.*inventory.html/);
   });
 
-  test('Invalid password', async ({ page }) => {
+  test('Login with Invalid credentials', async ({ page }) => {
     const loginPage = new LoginPage(page);
-    const loginSuccess = await loginPage.login(testData.validUser.username, 'wrong_password');
+    const loginSuccess = await loginPage.login(testData.invalidUser.username, testData.invalidUser.password);
     expect(loginSuccess).toBe(false);
     const errorMessage = await loginPage.getErrorMessage();
     expect(errorMessage).toContain('Username and password do not match');
   });
 
-  test('Invalid username', async ({ page }) => {
-    const loginPage = new LoginPage(page);
-    const loginSuccess = await loginPage.login('wrong_username', testData.validUser.password);
-    expect(loginSuccess).toBe(false);
-    const errorMessage = await loginPage.getErrorMessage();
-    expect(errorMessage).toContain('Username and password do not match');
-  });
-
-  test('Locked out user', async ({ page }) => {
-    const loginPage = new LoginPage(page);
-    const loginSuccess = await loginPage.login(testData.lockedUser.username, testData.lockedUser.password);
-    expect(loginSuccess).toBe(false);
-    const errorMessage = await loginPage.getErrorMessage();
-    expect(errorMessage).toContain('locked out');
-  });
+ 
 });
